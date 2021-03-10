c41a27c91121d520ee6aace0ca72f5ea332249ec
melakc <38559247+melakc@users.noreply.github.com>
2021-03-10 09:09:39 +0100

    first commit

---
+++ src/components/ContactMe.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+export default class ContactMe extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+
+    return (
+      <section id="contact">
+        <div className=" row section-head">
+          <div className="ten columns">
+            <p className="lead">Feel free to contact me.</p>
+          </div>
+        </div>
+        <div className="row">
+          <aside className="eigth columns footer-widgets">
+            <div className="widget">
+              <button variant="contained">Contact</button>
+            </div>
+          </aside>
+        </div>
+      </section>
+    );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,68 @@
+let resumeData = {
+  imagebaseurl: "",
+  name: "Melise Akcay",
+  role: "Frontend Developer and UX-Designer by choice",
+  linkedinId: "Your ID",
+  roleDescription:
+    " I like to learn and create Web-Applications in various parts of software development and like to learn about new technologies, follow UX Trends or simply create art in my free time.",
+
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/melise-akcay-344779193/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "instagram",
+      url: "https://www.instagram.com/melise_ak/",
+      className: "fa fa-instagram",
+    },
+  ],
+
+  aboutme:
+    "I am 23 years old and work as an frontend developer at BCM Solutions in Stuttgart. I have an Bachelor's Dregree in economic computer science and worked for three years at an Consultant Agency in Frnakfurt am Main. I believe that creativity is born and talent is learned.",
+  address: "Not gonna tell u where I live.",
+  education: [
+    {
+      UniversityName: "Duale Hochschule Baden-Würrtemberg Karlsruhe",
+      specialization: "Bachelor of science - Software Engineering",
+      MonthOfPassing: "Oct",
+      YearOfPassing: "2020",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "Cofinpro AG",
+      CompanyAdress: "Frankfurt am Main",
+      specialization: "Consulting ",
+      MonthOfLeaving: "Oct",
+      YearOfLeaving: "2020",
+    },
+    {
+      CompanyName: "BCM Solutions GmbH",
+      CompanyAdress: "Stuttgart",
+      specialization: "Consulting ",
+      YearOfLeaving: "today",
+    },
+  ],
+  skillsDescription: "I am really good at:",
+  skills: [
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS",
+    },
+    {
+      skillname: "JavaScript",
+    },
+    {
+      skillname: "Reactjs",
+    },
+    {
+      skillname: "Angular",
+    },
+  ],
+};
+
+export default resumeData;
